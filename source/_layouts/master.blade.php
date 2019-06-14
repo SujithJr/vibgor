08a1568076743208ae87a12850b56f6e0933643c
sujith kumar <sujithsrt10@gmail.com>
2019-06-14 18:28:54 +0530

    Changes

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Vibhgyar</title>
+        <link rel="icon" href="{{ $page->baseUrl }}/assets/images/icon-11.png">
         <link rel="stylesheet" href="{{ $page->baseUrl }}{{ mix('css/main.css', 'assets/build') }}">
         <link href="https://unpkg.com/ionicons@4.5.0/dist/css/ionicons.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://unpkg.com/flexboxgrid2@7.2.1/flexboxgrid2.css">
